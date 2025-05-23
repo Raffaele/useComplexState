aab7fe37722ba1cdc7bce02b45ecd4c0434ae539
Raffaele <mori.raffaele@gmail.com>
2025-05-23 11:00:13 +0100

    Usage simplified by merging the functionality of the 2nd and 3rd elements in the returned array

--- useComplexState.d.ts
+++ useComplexState.d.ts
@@ -1,11 +1,14 @@
 type SetterArgument<Value> = Value | ((value: Value) => Value);
-type SetFullState<State> = (state: SetterArgument<State>) => State;
-type SetPartialState<State> = <Key extends keyof State>(key: Key, value: SetterArgument<State[Key]>) => void;
+type SetFullState<State> = (state: SetterArgument<State>) => void;
+
+type SetComplexState<State> = {
+  (state: SetterArgument<State>): void;
+  <Key extends keyof State>(key: Key, value: SetterArgument<State[Key]>): void;
+}
 
 type UseComplexStateReturn<State> = [
   State,
-  SetPartialState<State>,
-  SetFullState<State>
+  SetComplexState<State>
 ];
 
 declare function useComplexState<State>(initialState: State): UseComplexStateReturn<State>;
